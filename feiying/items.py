3c5eead5c2f106a314d3fdf5e621c5287e9930bc
huuguanghui <huuguanghui@gmail.com>
2012-02-18 22:44:56 +0800

    save episode to db

--- feiying/items.py
+++ feiying/items.py
@@ -8,7 +8,22 @@ from scrapy import log
 from scrapy.exceptions import DropItem
 from scrapy.item import Item, Field
 
-class FeiyingItem(Item):
+
+class ItemProcessor:
+    def _func_list(self):
+        return []
+    
+    def process(self, pipe, spider):
+        r = None
+        for f in self._func_list():
+            r = f(pipe, spider)
+            if isinstance(r, Item):
+                continue
+            else:
+                break
+        return r
+
+class FeiyingItem(Item, ItemProcessor):
     # define the fields for your item here like:
     # name = Field()
     title = Field()
@@ -16,6 +31,7 @@ class FeiyingItem(Item):
     source_id = Field()
     image_url = Field()
     category = Field()
+
 
 class FyVideoItem(FeiyingItem):
     time = Field()
@@ -41,16 +57,6 @@ class FySeriesItem(FeiyingItem):
     def _func_list(self):
         return [self._save_db]
 
-    def process(self, pipe, spider):
-        r = None
-        for f in self._func_list():
-            r = f(pipe, spider)
-            if isinstance(r, Item):
-                continue
-            else:
-                break
-        return r
-
     def _save_db(self, pipe, spider):
         status = self._check_db(pipe, spider)
         if status == 0:
@@ -65,7 +71,6 @@ class FySeriesItem(FeiyingItem):
     def _check_db(self, pipe, spider):
         sql = "SELECT id, episode_count, episode_all FROM fy_tv_series WHERE source_id = ?"
         param = (self['source_id'][0],)
-        log.msg('source_id = %s' % self['source_id'][0])
         r = None
         with pipe.db_conn.cursor() as cursor:
             cursor.execute(sql, param)
@@ -115,11 +120,51 @@ class FySeriesItem(FeiyingItem):
             cursor.execute(sql, param)
                
 
-class FyEpisodeItem(Item):
+class FyEpisodeItem(Item, ItemProcessor):
     source_id = Field()
     episode_index = Field()
     image_url = Field()
     video_url = Field()
     size = Field()
     time = Field()
+
+    def _func_list(self):
+        return [self._save_db]
+
+    def _save_db(self, pipe, spider):
+        r = self._check_db(pipe, spider)
+        if r == 0:
+            self._insert(pipe, spider)
+            return self
+        else:
+            return DropItem('This item is already in database')
+
+    def _check_db(self, pipe, spider):
+        sql = "SELECT id FROM fy_tv_episode WHERE source_id=? AND episode_index=?"
+        param = (self['source_id'][0], self['episode_index'][0])
+        r = None
+        with pipe.db_conn.cursor() as cursor:
+            cursor.execute(sql, param)
+            r = cursor.fetchone()
+
+        if r==None:
+            return 0 # this episode is not in db
+        else:
+            return 1 # this episode is already in db
+
+    def _insert(self, pipe, spider):
+        sql = """
+            INSERT INTO fy_tv_episode (time, size, source_id, episode_index, image_url,
+            video_url) VALUES (?,?,?,?,?,?)"""
+        param = (
+            self['time'][0],
+            self['size'][0],
+            self['source_id'][0],
+            self['episode_index'][0],
+            self['image_url'][0],
+            self['video_url'][0])
+
+        with pipe.db_conn.cursor() as cursor:
+            cursor.execute(sql, param)
+
 
